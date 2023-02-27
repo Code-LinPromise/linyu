34adfbc9b594142b0f2ded8eaf8ea0abc9618e30
Promise <326661445@qq.com>
2023-02-27 18:00:30 +0800

    提取MainLayout组件

---
+++ src/layouts/MainLayout/index.tsx
@@ -0,0 +1,23 @@
+import { defineComponent, PropType } from 'vue';
+import { NavBar } from '../../components/NavBar';
+import s from './style.module.scss';
+export const MainLayout = defineComponent({
+  setup: (props, context) => {
+    return () =>(
+    <>
+        <NavBar>
+            {
+                {
+                    title:()=>context.slots.title?.(),
+                    icon:()=>context.slots.icon?.() 
+                }
+            }
+        </NavBar>
+        {
+            context.slots.default?.()
+        }
+    </>
+
+  )
+}
+})
--- src/pages/Strat/index.tsx
+++ src/pages/Strat/index.tsx
@@ -4,8 +4,8 @@ import { FloatButton } from '../../components/FloatButton';
 import s from "./style.module.scss"
 import { Center } from '../../components/Center';
 import { Icon } from '../../components/Icon';
-import { NavBar } from '../../components/NavBar';
 import { Overlay } from '../../components/Overlay';
+import { MainLayout } from '../../layouts/MainLayout';
 export const Start = defineComponent({
     
   setup: (props, context) => {
@@ -17,23 +17,28 @@ export const Start = defineComponent({
         isShow.value=false
     }
     return () => (
-    <div>
-        <menu>
-            <NavBar class={s.menu}>
-                {{
-                    icon:()=> <Icon name="menu" class={s.menuIcon} onClick={setOverlay}></Icon>,
-                    title:()=> <span class={s.menuFont}>山竹记账</span>
-                }}
-            </NavBar></menu>
-        <Center direction='vertical' class={s.icon_wrapper}>
-            <Icon name="piggy" class={s.icon}></Icon>
-        </Center>
-        <div class={s.box}>
-                <Button class={s.button}>开始记账</Button>
-        </div>
-        {isShow.value && <Overlay close={closeOverlay}></Overlay>}
-        <FloatButton></FloatButton>
-    </div>
-    )
+      <div>
+        <MainLayout>
+          {{
+            icon: () => (
+              <Icon name="menu" class={s.menuIcon} onClick={setOverlay}></Icon>
+            ),
+            title: () => <span class={s.menuFont}>山竹记账</span>,
+            default: () => (
+              <>
+                <Center direction="vertical" class={s.icon_wrapper}>
+                  <Icon name="piggy" class={s.icon}></Icon>
+                </Center>
+                <div class={s.box}>
+                  <Button class={s.button}>开始记账</Button>
+                </div>
+                {isShow.value && <Overlay close={closeOverlay}></Overlay>}
+                <FloatButton></FloatButton>
+              </>
+            ),
+          }}
+        </MainLayout>
+      </div>
+    );
   }
 })
