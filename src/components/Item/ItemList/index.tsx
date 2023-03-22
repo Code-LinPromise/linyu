31db635108d10b25da6c7d013ba0cc3dc9e65e74
Promise <326661445@qq.com>
2023-03-22 21:40:43 +0800

    完成ItemList组件

--- src/components/Item/ItemList/index.tsx
+++ src/components/Item/ItemList/index.tsx
@@ -1,14 +1,31 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
+import { MainLayout } from '../../../layouts/MainLayout';
+import { Icon } from '../../Icon';
+import { Tab, Tabs } from '../../Tabs';
 import s from './style.module.scss';
 export const ItemList = defineComponent({
-  props: {
-    name: {
-      type: String as PropType<string>
-    }
-  },
-  setup: (props, context) => {
-    return () => (
-      <div class={s.wrapper}>hi item create</div>
-    )
-  }
+    props:{
+        name:{
+            type:String as PropType<string>
+        }
+    },
+   setup: (props, context) => {
+    const refSelected=ref("本月")
+    return ()=>(
+        <MainLayout>
+            {{
+                icon:()=><Icon name="menu" class={s.icon}></Icon>,
+                title:()=><span class={s.font}>山竹记账</span>,
+                default: () => (
+                    <Tabs v-model:selected={refSelected.value}>
+                        <Tab name='本月'>1</Tab>
+                        <Tab name='上月'>2</Tab>
+                        <Tab name='今年'>3</Tab>
+                        <Tab name='自定义时间'>4</Tab>
+                    </Tabs>
+                )
+            }}
+        </MainLayout>  
+    )
+}
 })
