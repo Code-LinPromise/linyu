8498aef68c23e54148ede6674e76b844e2f73cb4
Promise <326661445@qq.com>
2023-04-05 15:14:28 +0800

    加载第一个tag

--- src/components/Item/ItemCreate/index.tsx
+++ src/components/Item/ItemCreate/index.tsx
@@ -5,6 +5,7 @@ import { Tabs, Tab } from '../../Tabs';
 import s from './style.module.scss';
 import { useRouter } from 'vue-router';
 import { InputPad } from '../../InputPad';
+import { Button } from '../../Button';
 import { http } from '../../../shared/Http';
 export const ItemCreate = defineComponent({
 
@@ -18,14 +19,18 @@ export const ItemCreate = defineComponent({
     const back=()=>{
       router.back()
     }
+	const refPage = ref(0)
+    const refHasMore = ref(false)
 	const refExpensesTags = ref<Tag[]>([])
 	const refIncomeTags = ref<Tag[]>([])
 	onMounted(async () => {
-		const response = await http.get<{ resources: Tag[] }>('/tags', {
+		const response = await http.get<Resources<Tag>>('/tags', {
 		  kind: 'expenses',
 		  _mock: 'tagIndex'
 		})
-		refExpensesTags.value = response.data.resources
+		const { resources, pager } = response.data
+      	refExpensesTags.value = resources
+      	refHasMore.value = (pager.page - 1) * pager.per_page + resources.length < pager.count
 	  })
 	  
 	  onMounted(async () => {
@@ -44,27 +49,35 @@ export const ItemCreate = defineComponent({
 				default: () => (
 					<>
 					<div class={s.wrapper}>
-					  <Tabs v-model:selected={refKind.value} onUpdate:selected={()=>console.log(1)} class={s.tabs}>
-						<Tab name="支出" class={s.tags_wrapper}>
-						  <div class={s.tag}>
-							<div class={s.sign}>
-							  <Icon name="add" class={s.createTag} />
-							</div>
-							<div class={s.name}>
-							  新增
-							</div>
-						  </div>
-						  {refExpensesTags.value.map(tag =>
-							<div class={[s.tag, s.selected]}>
-							  <div class={s.sign}>
-								{tag.sign}
-							  </div>
-							  <div class={s.name}>
-								{tag.name}
-							  </div>
-							</div>
-						  )}
-						</Tab>
+						<Tabs v-model:selected={refKind.value} onUpdate:selected={() => console.log(1)} class={s.tabs}>
+							<Tab name="支出">
+								<div class={s.tags_wrapper}>
+									<div class={s.tag}>
+										<div class={s.sign}>
+											<Icon name="add" class={s.createTag} />
+										</div>
+										<div class={s.name}>
+											新增
+										</div>
+									</div>
+									{refExpensesTags.value.map(tag =>
+										<div class={[s.tag, s.selected]}>
+											<div class={s.sign}>
+												{tag.sign}
+											</div>
+											<div class={s.name}>
+												{tag.name}
+											</div>
+										</div>
+									)}
+								</div>
+								<div class={s.more}>
+									{refHasMore.value ?
+										<Button class={s.loadMore}>加载更多</Button> :
+										<span class={s.noMore}>没有更多</span>
+									}
+								</div>
+							</Tab>
 						<Tab name="收入" class={s.tags_wrapper}>
 						  <div class={s.tag}>
 							<div class={s.sign}>
