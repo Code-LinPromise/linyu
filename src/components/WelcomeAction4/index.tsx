a94e75562034d1fd69e85844300b66ad0fcbe11e
Promise <326661445@qq.com>
2023-04-05 14:07:58 +0800

    修复点击开启应用后还要看welcome

--- src/components/WelcomeAction4/index.tsx
+++ src/components/WelcomeAction4/index.tsx
@@ -3,11 +3,15 @@ import { RouterLink } from 'vue-router';
 import { SkipFeatures } from '../SkipFeatures';
 import s from "../WelcomeLayout/style.module.scss"
 export const WelcomeAction4 = defineComponent({
+  
   setup: (props, context) => {
+  const onClick = () => {
+    localStorage.setItem('skipFeatures', 'yes')
+  }
     return () => (
       <div class={s.action}>
         <SkipFeatures class={s.fake}/>
-        <RouterLink to="/start">开启应用</RouterLink>
+        <span onClick={onClick}><RouterLink to="/start">开启应用</RouterLink></span>
         <SkipFeatures class={s.fake}/>
       </div>
     );
